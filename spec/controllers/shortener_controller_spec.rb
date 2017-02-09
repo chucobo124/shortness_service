6757ab0b4ca4fdede29e7aece5b23de61ab69cee
ChenLucas <chucobo5219@gmail.com>
2017-02-09 22:36:38 +0800

    Add test case

---
+++ spec/controllers/shortener_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ShortenerController, type: :controller do
+  describe '#index' do
+  end
+
+  describe '#new' do
+  end
+
+  describe '#show' do
+  end
+
+  describe '#create' do
+    let(:params_data) do
+    end
+    subject { post :create }
+    it 'save the user ip address' do
+      subject
+    end
+    it 'save the user browser'
+    it 'save the user gender'
+    it 'save the user ages'
+  end
+end
